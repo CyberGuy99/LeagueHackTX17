d980afc2452a1d5910976a285995a86ecdc8320c
Sean Yu <seantyu@utexas.edu>
2017-10-28 18:02:41 -0500

    updated apiRequests.py

--- apiRequests.py
+++ apiRequests.py
@@ -4,14 +4,14 @@ import requests
 
 APIKey = "RGAPI-b4e2938b-12ff-4321-a0cc-b5d651c381c5"
 
-def getSummonerData(summonerName):
+def getSummonerData(summoner):
     '''
-    summonerName:string, no spaces
+    summoner: int or string, can be either accountId, summonerName, or summonerId
 
-    returns: JSON file containing accountId, id, name, profileIconId
-    revisionDate, and summonerLevel
+    returns: summonerDTO(a JSON file representing a summoner as specified in summoner-v3)
+    as a dictionary
     '''
-    URL = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName
+    URL = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summoner
     r = requests.get(URL, params = {"api_key":APIKey})
     return r.json()
 
