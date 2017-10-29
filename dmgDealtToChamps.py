e8a868b9017e9510751394528c4d820d57e88e87
Rushil <rushilcd@gmail.com>
2017-10-28 21:31:58 -0500

    "fixed math import"

--- dmgDealtToChamps.py
+++ dmgDealtToChamps.py
@@ -1,3 +1,4 @@
+import math
 '''
 Goal: Display 2 graphs, 1 showing total winning team dmg dealt to champs across times, other showing total losing team dmg dealt to champs across times
 Input: matchData is the dictionary you are taking in to analyze from the API
@@ -6,7 +7,7 @@ More detailed output: [2,466,333]
 '''
 #0: <=20, 1: 20-25, 2: 25-30, 3: 30-35, 4:35-40, 5: 40-45, 6: 45-50, 7: 50-55, 8: 55<
 def dmgDealtToChamps(matchData):
-    gameDuration = round(matchData["gameDuration"]/60)
+    gameDuration = math.round(matchData["gameDuration"]/60)
     winningTeamDmg = 0
     losingTeamDmg = 0
     for participant in matchData["participants"]:
@@ -19,5 +20,5 @@ def dmgDealtToChamps(matchData):
     elif (gameDuration > 55):
         gameLength = 8
     else:
-        gameLength = ceil((gameDuration-20)/5)
+        gameLength = math.ceil((gameDuration-20)/5)
     return [gameLength, winningTeamDmg, losingTeamDmg]
